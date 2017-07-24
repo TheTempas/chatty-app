e11bdb19a4f363c1049202c08d1d6f22c5a06817
Nicole <nicole.tempas@gmail.com>
2017-07-24 21:15:47 +0000

    Add HTML/ CSS using React, main page renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div className="wrapper">
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
+
 export default App;

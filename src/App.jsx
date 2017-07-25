e73b326dfbd2bddfc87610a406c9f80d92ac3542
Nicole <nicole.tempas@gmail.com>
2017-07-25 16:42:11 +0000

    React renders incoming data using props

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,34 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+          currentUser: {name: "Joe"},
+          messages: [
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?"
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+  }
+}
+
   render() {
     return (
       <div className="wrapper">
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,9 +4,13 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+
+  let messages = this.props.messages.map((message, index) => {
+    return <Message key={message.id} username={message.username} content={message.content} />
+    })
     return (
       <main className="messages">
-        <Message />
+        {messages}
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>
